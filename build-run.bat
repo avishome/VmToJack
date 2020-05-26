ed93dadd7924083c65715503987b1f7829b2155e
Avishai <avishome@gmail.com>
2020-05-26 11:06:31 +0300

    replace class

--- build-run.bat
+++ build-run.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files\Ballerina\bin\ballerina.bat" build -a
-call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\Parser.jar
+call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\Tokenizer.jar
 pause
