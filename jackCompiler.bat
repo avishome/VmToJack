23ed24bc065a523e726bc7249cc37a0b9e9bd501
ElishaMayer <44092325+ElishaMayer@users.noreply.github.com>
2020-06-17 22:21:13 +0300

    fix script

--- jackCompiler.bat
+++ jackCompiler.bat
@@ -1,3 +1,5 @@
-call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\Tokenizer.jar files
-call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\Parser.jar files
-call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\CodeWriter.jar files
+@echo off
+call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\Tokenizer.jar %1
+call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\Parser.jar %1
+call "C:\Program Files\Ballerina\bin\ballerina.bat" run target\bin\CodeWriter.jar %1
+DEL %1\*.xml
